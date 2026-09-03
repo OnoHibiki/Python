1dae07283e5d78cff6d82db992cbc620115dfc7e
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-09-03 22:21:07 +0900

    feat:Practice/01_variables.py追加

---
+++ Practice/01_variables.py
@@ -0,0 +1,26 @@
+# やる必要ある？基本「変数」
+
+name = "hibiki"
+age = 32
+
+print(name)
+print(age)
+print(f"名前は{name}です")
+print(f"年齢は{age}です") #Javaみたいに勝手にstrとintを+できない
+
+# 型チェック
+
+print(type(name)) #str
+print(type(age)) #int
+
+# 演算
+
+a = 8
+b = 14
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) #整数除算
+print(a % b)
